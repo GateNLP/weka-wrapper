63d58cd55a1681f8057336e70ac559f695694e2d
Johann Petrak <johann.petrak@gmail.com>
2016-07-11 13:38:07 +0200

    Make it work on Windows.

--- bin/wekaWrapperApply.cmd
+++ bin/wekaWrapperApply.cmd
@@ -2,11 +2,12 @@
 
 SET ROOTDIR=%1
 SET model=%2
-SET header=%2
+SET header=%3
 
 SET java=java
 if not [%JRE_HOME%]==[] SET java=%JRE_HOME%\bin\java.exe
 if not [%JAVA_HOME%]==[] SET java=%JAVA_HOME%\bin\java.exe
 : TODO not sure if forward or backward slashes and also, if we need quotes and where
 : could be we need double quotes around everything except the star
+echo 1>&2 %java% -cp %ROOTDIR%/target/*;%ROOTDIR%/target/dependency/* gate.lib.wekawrapper.WekaApplication %model% %header%
 %java% -cp %ROOTDIR%/target/*;%ROOTDIR%/target/dependency/* gate.lib.wekawrapper.WekaApplication %model% %header%
